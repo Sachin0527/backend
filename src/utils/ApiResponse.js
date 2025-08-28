287f990fdbb3e6ea1b36f8a53e142b9af3e7f8bc
Sachin0527 <sachinkumar27demon@gmail.com>
2025-08-28 19:51:06 +0530

    util code

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,13 @@
+class ApiResponse{
+    constructor(statusCode,
+        data, 
+        messages ="Success"){
+        this.statusCode=statusCode
+        this.data=data
+        this.messages=messages
+        this.success=statusCode<400
+
+    }
+}
+
+export {ApiResponse}
